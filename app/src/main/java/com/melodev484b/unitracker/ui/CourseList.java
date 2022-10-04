7d1783dd6a787bf4a47b476b754420a3b4e605f2
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-03 22:20:43 -0400

    Create CourseList.java

---
+++ app/src/main/java/com/melodev484b/unitracker/ui/CourseList.java
@@ -0,0 +1,14 @@
+package com.melodev484b.unitracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+    }
+}
