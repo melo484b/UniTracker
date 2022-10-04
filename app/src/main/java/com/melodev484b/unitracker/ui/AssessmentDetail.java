243e7de8da404950326e00a3db857fc67dd7798f
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-03 22:16:00 -0400

    Create AssessmentDetail.java

---
+++ app/src/main/java/com/melodev484b/unitracker/ui/AssessmentDetail.java
@@ -0,0 +1,16 @@
+package com.melodev484b.unitracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.melodev484b.unitracker.R;
+
+public class AssessmentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_detail);
+    }
+}
