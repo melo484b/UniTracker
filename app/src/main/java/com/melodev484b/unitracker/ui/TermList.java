cf5c36d22accb17c82f8b1f19b663c57cffea8a0
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-03 22:20:40 -0400

    Create TermList.java

---
+++ app/src/main/java/com/melodev484b/unitracker/ui/TermList.java
@@ -0,0 +1,14 @@
+package com.melodev484b.unitracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
