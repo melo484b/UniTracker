00cdf266b49fd9355e512b91c887df78d698ec07
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-05 12:03:15 -0400

    Implement TermDao CRUD

--- app/src/main/java/com/melodev484b/unitracker/dao/TermDao.java
+++ app/src/main/java/com/melodev484b/unitracker/dao/TermDao.java
@@ -1,4 +1,25 @@
 package com.melodev484b.unitracker.dao;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.melodev484b.unitracker.entity.Term;
+
+import java.util.List;
+
 public interface TermDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Term term);
+
+    @Update
+    void update(Term term);
+
+    @Delete
+    void delete(Term term);
+
+    @Query("SELECT * FROM terms ORDER BY termId ASC")
+    List<Term> getAllTerms();
 }
