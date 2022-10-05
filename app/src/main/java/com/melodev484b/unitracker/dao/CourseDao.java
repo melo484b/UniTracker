72933d02d8b8c6bf032f94aefe02b85e35d05298
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-05 12:03:06 -0400

    Implement CourseDao CRUD

--- app/src/main/java/com/melodev484b/unitracker/dao/CourseDao.java
+++ app/src/main/java/com/melodev484b/unitracker/dao/CourseDao.java
@@ -1,4 +1,25 @@
 package com.melodev484b.unitracker.dao;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.melodev484b.unitracker.entity.Course;
+
+import java.util.List;
+
 public interface CourseDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Course course);
+
+    @Update
+    void update(Course course);
+
+    @Delete
+    void delete(Course course);
+
+    @Query("SELECT * FROM courses ORDER BY courseId ASC")
+    List<Course> getAllCourses();
 }
