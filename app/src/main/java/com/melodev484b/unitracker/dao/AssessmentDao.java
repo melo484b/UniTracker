61f980c22dd86e087c28f3b509888feecbb5342d
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-05 12:02:53 -0400

    Implement AssessmentDao CRUD

--- app/src/main/java/com/melodev484b/unitracker/dao/AssessmentDao.java
+++ app/src/main/java/com/melodev484b/unitracker/dao/AssessmentDao.java
@@ -1,4 +1,25 @@
 package com.melodev484b.unitracker.dao;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.melodev484b.unitracker.entity.Assessment;
+
+import java.util.List;
+
 public interface AssessmentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Assessment assessment);
+
+    @Update
+    void update(Assessment assessment);
+
+    @Delete
+    void delete(Assessment assessment);
+
+    @Query("SELECT * FROM assessments ORDER BY assessmentId ASC")
+    List<Assessment> getAllAssessments();
 }
