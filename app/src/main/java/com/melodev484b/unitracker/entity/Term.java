4656fecde752ee737d9c3709008c240d44073359
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-05 12:31:19 -0400

    feat: Implement Term

--- app/src/main/java/com/melodev484b/unitracker/entity/Term.java
+++ app/src/main/java/com/melodev484b/unitracker/entity/Term.java
@@ -1,7 +1,62 @@
 package com.melodev484b.unitracker.entity;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity
 public class Term {
+    @PrimaryKey(autoGenerate = true)
+    int termId;
+    String title;
+    String startDate;
+    String endDate;
+
+    public Term(int termId, String title, String startDate, String endDate) {
+        this.termId = termId;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Term{" +
+                "termId=" + termId +
+                ", title='" + title + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+
+    public int getTermId() {
+        return termId;
+    }
+
+    public void setTermId(int termId) {
+        this.termId = termId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
