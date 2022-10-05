5d768dc7c5982e88476784d40013f0c4f3a06cef
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-05 12:44:55 -0400

    feat: Implement RoomDatabase

--- app/src/main/java/com/melodev484b/unitracker/db/UniTrackerDatabaseBuilder.java
+++ app/src/main/java/com/melodev484b/unitracker/db/UniTrackerDatabaseBuilder.java
@@ -1,4 +1,35 @@
 package com.melodev484b.unitracker.db;
 
-public class UniTrackerDatabaseBuilder {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.melodev484b.unitracker.dao.AssessmentDao;
+import com.melodev484b.unitracker.dao.CourseDao;
+import com.melodev484b.unitracker.dao.TermDao;
+import com.melodev484b.unitracker.entity.Assessment;
+import com.melodev484b.unitracker.entity.Course;
+import com.melodev484b.unitracker.entity.Term;
+
+@Database(entities = {Assessment.class, Course.class, Term.class}, version = 1, exportSchema = false)
+public abstract class UniTrackerDatabaseBuilder extends RoomDatabase {
+    public abstract AssessmentDao assessmentDao();
+    public abstract CourseDao courseDao();
+    public abstract TermDao termDao();
+
+    private static volatile UniTrackerDatabaseBuilder INSTANCE;
+    static UniTrackerDatabaseBuilder getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (UniTrackerDatabaseBuilder.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), UniTrackerDatabaseBuilder.class, "UniTracker.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
