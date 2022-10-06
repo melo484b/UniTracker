ec04565639bd9f24dfe13395e09f720a6c4c2b62
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-06 12:50:29 -0400

    Update activity_main.xml

--- app/src/main/java/com/melodev484b/unitracker/ui/MainActivity.java
+++ app/src/main/java/com/melodev484b/unitracker/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.melodev484b.unitracker.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.melodev484b.unitracker.R;
 
@@ -12,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onTermButton(View view) {
+
+    }
+
+    public void onAssessmentButton(View view) {
+
+    }
+
+    public void onCourseButton(View view) {
+
     }
 }
