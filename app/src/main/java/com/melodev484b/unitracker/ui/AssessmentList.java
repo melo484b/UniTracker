cb7edb18ecf3d8d1597e3b739ad52ca57ee9286a
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-10 13:14:47 -0400

    Update AssessmentList

--- app/src/main/java/com/melodev484b/unitracker/ui/AssessmentList.java
+++ app/src/main/java/com/melodev484b/unitracker/ui/AssessmentList.java
@@ -1,10 +1,20 @@
 package com.melodev484b.unitracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.melodev484b.unitracker.R;
+import com.melodev484b.unitracker.db.Repository;
+import com.melodev484b.unitracker.entity.Assessment;
+
+import java.util.List;
 
 public class AssessmentList extends AppCompatActivity {
 
@@ -12,5 +22,33 @@ public class AssessmentList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        RecyclerView recyclerView = findViewById(R.id.assessment_recycler);
+        Repository repo = new Repository(getApplication());
+        List<Assessment> assessments = repo.getmAllAssessments();
+        final AssessmentAdapter adapter = new AssessmentAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setAssessments(assessments);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_assessment_list, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAssessmentActionButton(View view) {
+        Intent intent = new Intent(this, AssessmentEdit.class);
+        startActivity(intent);
     }
 }
