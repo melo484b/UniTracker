24c5981167f6453d171c53bd66cf347f716965b6
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-10 13:13:21 -0400

    Implement onClick methods for MainActivity

--- app/src/main/java/com/melodev484b/unitracker/ui/MainActivity.java
+++ app/src/main/java/com/melodev484b/unitracker/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.melodev484b.unitracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onTermButton(View view) {
-
+        Intent intent = new Intent(this, TermList.class);
+        startActivity(intent);
     }
 
     public void onAssessmentButton(View view) {
-
+        Intent intent = new Intent(this, AssessmentList.class);
+        startActivity(intent);
     }
 
     public void onCourseButton(View view) {
-
+        Intent intent = new Intent(this, CourseList.class);
+        startActivity(intent);
     }
 }
